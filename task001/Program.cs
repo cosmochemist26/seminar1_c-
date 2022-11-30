caf64dbb740a44b24f517434dcef680451134d1a
Lyubov <love.rodionova.spb@gmail.com>
2022-11-30 22:20:11 +0300

    add solution for tasks

---
+++ task001/Program.cs
@@ -0,0 +1,20 @@
+﻿//Считываем данные с консоли
+string? num1Line = Console.ReadLine(); 
+string? num2Line = Console.ReadLine(); 
+
+//Проверяем, чтобы данные были не пустыми
+if (num1Line!=null&&num2Line!=null);
+{
+    //Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    //Находим произведение чисел
+    int outResult = num1*num2;
+    //или int outResult = (int)Math.Pow(inputnum,2);
+
+
+    //Выводим данные в консоль
+    Console.WriteLine(outResult);
+    //или Console.Writeline(MathPow(int.Parse(inputLine),2));
+}
---
+++ task002/Program.cs
@@ -0,0 +1,23 @@
+﻿//Считываем данные с консоли
+string? num1Line = Console.ReadLine(); 
+string? num2Line = Console.ReadLine(); 
+
+//Проверяем, чтобы данные были не пустыми
+if (num1Line != null && 
+    num2Line != null);
+{
+    //Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    //Находим произведение чисел
+    if (num2 * num2 == num1)
+    {
+        Console.WriteLine("Первое число квадрат второго");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не квадрат второго");
+    }
+
+}
