8c1dcc0a504c52dd30cacd92cdf9de7a8c2a0a2b
Lyubov <love.rodionova.spb@gmail.com>
2022-12-02 20:37:33 +0300

    hometask 006 added

---
+++ HW006/Program.cs
@@ -0,0 +1,25 @@
+﻿//Считываем данные с консоли
+string? num1Line = Console.ReadLine(); 
+
+
+//Проверяем, чтобы данные были не пустыми
+if (num1Line != null)
+{
+
+    //Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    
+    //Ищем остаток от деления
+
+    int mod = num1 % 2; 
+
+    if (mod == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+    
+}
