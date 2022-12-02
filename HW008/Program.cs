2dee661dee81e033990432f7122344ce79f83b50
Lyubov <love.rodionova.spb@gmail.com>
2022-12-02 20:49:49 +0300

    hometask 008 added

---
+++ HW008/Program.cs
@@ -0,0 +1,23 @@
+﻿//Считываем данные с консоли
+string? nLine = Console.ReadLine(); 
+
+//Проверяем, чтобы данные были не пустыми
+if (nLine != null)
+{    
+    //Парсим введенное число
+    int n = int.Parse(nLine);
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        //Ищем остаток от деления
+
+        int mod = i % 2; 
+
+        if (mod == 0)
+        {
+            Console.WriteLine(i);
+        }
+        
+    }
+
+}
