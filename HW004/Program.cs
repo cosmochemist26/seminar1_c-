5782104943e0a21ae8bd5e61e81d770204a1efce
Lyubov <love.rodionova.spb@gmail.com>
2022-12-02 20:26:32 +0300

    hometask 004 added

---
+++ HW004/Program.cs
@@ -0,0 +1,29 @@
+﻿//Считываем данные с консоли
+string? num1Line = Console.ReadLine(); 
+string? num2Line = Console.ReadLine(); 
+string? num3Line = Console.ReadLine(); 
+
+//Проверяем, чтобы данные были не пустыми
+if (num1Line != null && num2Line != null && num3Line != null)
+{
+
+    //Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+    int num3 = int.Parse(num3Line);
+
+    // Вводим переменную
+    int max = num1;
+
+    //Сравниваем числа
+    if (num2 > max)
+    {
+        max = num2;
+    }
+    if (num3 > max)
+    {
+        max = num3;
+    }
+    
+    Console.WriteLine(max);
+}
