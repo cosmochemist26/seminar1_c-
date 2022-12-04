c2f6ac61c7b92385bceb1ef21f65af1560c39f5d
Lyubov <love.rodionova.spb@gmail.com>
2022-12-04 20:39:11 +0300

    hometask015 added

---
+++ HW015/Program.cs
@@ -0,0 +1,19 @@
+﻿//Считываем данные с консоли
+int day = int.Parse(Console.ReadLine());
+
+if (day >= 1 && 
+    day <= 7)
+{
+    if (day > 5)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
