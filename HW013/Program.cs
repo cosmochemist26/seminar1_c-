e3eb81af6250cdf8f358bb7bd1429720bbd5a834
Lyubov <love.rodionova.spb@gmail.com>
2022-12-04 20:19:11 +0300

    hometask013 added

---
+++ HW013/Program.cs
@@ -0,0 +1,14 @@
+﻿//Считываем данные с консоли в массив символов
+char[] number = Console.ReadLine().ToCharArray();
+
+if (number.Length >= 3) 
+{
+    //Выводим третью цифру в числе
+    Console.WriteLine(number[2]);
+}
+else
+{
+    //Выводим ответ
+    Console.WriteLine("Третьей цифры нет");
+}
+
