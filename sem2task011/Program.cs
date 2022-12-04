646a742eecb386745a830541c3f6151feb68bd3b
Lyubov <love.rodionova.spb@gmail.com>
2022-12-04 19:59:21 +0300

    semtask011 added

---
+++ sem2task011/Program.cs
@@ -0,0 +1,17 @@
+﻿// Создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+
+// получаем новое случайное число
+int number = numberSintezator.Next(100,1000);
+
+// Выводим данные в консоль
+Console.WriteLine(number);
+
+// Выводим первое и последнее число из трехзначного поотдельности
+//int firstNumber = number/100;
+//int secondNumber = number%10;
+
+int result = (number/100)*10 + number%10;
+
+//Выводим данные в консоль 
+Console.WriteLine(result);
