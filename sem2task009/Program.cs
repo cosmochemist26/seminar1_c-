af561cbaf1cf6477cfebaaad036539afb14efb20
Lyubov <love.rodionova.spb@gmail.com>
2022-12-04 19:44:20 +0300

    semtask009 added

---
+++ sem2task009/Program.cs
@@ -0,0 +1,24 @@
+﻿// Создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+
+// получаем новое случайное число
+int number = numberSintezator.Next(10,100);
+
+// Выводим данные в консоль
+Console.WriteLine(number);
+
+// Выводим первое и второе число поотдельности
+int firstNumber = number/10;
+int secondNumber = number%10;
+
+// Сравниваем числа
+if(firstNumber > secondNumber)
+{
+    //Выводим данные в консоль 
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    //Выводим данные в консоль 
+    Console.WriteLine(secondNumber);
+}
