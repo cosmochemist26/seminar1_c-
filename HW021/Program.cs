4e7eb4a710b96237a214a3e8058a60a13ba4b45f
Lyubov <love.rodionova.spb@gmail.com>
2022-12-07 21:31:50 +0300

    hometask021 added

---
+++ HW021/Program.cs
@@ -0,0 +1,35 @@
+﻿double[] ReadPointCoord(string msg)
+{
+    Console.WriteLine(msg);
+
+    string Input = Console.ReadLine() ?? "0 0 0";
+    string[] StringCoords = Input.Split(" ");
+
+    List<double> Coords = new List<double>();
+
+    foreach (var StringCoord in StringCoords) 
+    {
+        Coords.Add(double.Parse(StringCoord));
+    }
+
+    return Coords.ToArray();
+}
+
+double CalcDistance(double[] a, double[] b)
+{
+    double distance = 0.0;
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        distance += Math.Pow(b[i] - a[i], 2);
+    }
+    
+    return Math.Round(Math.Sqrt(distance), 2);
+    
+}
+
+double[] a = ReadPointCoord("Введите координаты точки А через пробел:");
+double[] b = ReadPointCoord("Введите координаты точки B через пробел:");
+
+Console.WriteLine(CalcDistance(a, b));
+
