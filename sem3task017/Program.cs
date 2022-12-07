e32803fdfca4ba96c4c9cc53654baf2dffc1e173
Lyubov <love.rodionova.spb@gmail.com>
2022-12-07 20:16:38 +0300

    sem3task017 changed

--- sem3task017/Program.cs
+++ sem3task017/Program.cs
@@ -1,16 +1,35 @@
 ﻿//Ввод координат
 
-Console.WriteLine("Введите координату X");
-int coordX = int.Parse(Console.ReadLine()??"0");
+//Console.WriteLine("Введите координату X");
+//int coordX = int.Parse(Console.ReadLine()??"0");
 
-Console.WriteLine("Введите координату Y");
-int coordY = int.Parse(Console.ReadLine()??"0");
+//Console.WriteLine("Введите координату Y");
+//int coordY = int.Parse(Console.ReadLine()??"0");
 
-if(coordX>0&&coordY>0) Console.WriteLine("Точка в четверти I");
+//Рефакторинг кода
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+// Метод определяет четверть
+void PrintQuterTest()
+{
+  if(coordX>0&&coordY>0) Console.WriteLine("Точка в четверти I");
 
 if(coordX>0&&coordY<0) Console.WriteLine("Точка в четверти II");
 
 if(coordX<0&&coordY<0) Console.WriteLine("Точка в четверти III");
 
 if(coordX<0&&coordY>0) Console.WriteLine("Точка в четверти IV");
+ 
+}
+
+
+int coordX = ReadData("Введите координату X");
+int coordY = ReadData("Введите координату Y");
+
+PrintQuterTest()
 
