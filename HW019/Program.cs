f96cf5ce4570b382789ec4815f95676acefd8c74
Lyubov <love.rodionova.spb@gmail.com>
2022-12-07 20:43:49 +0300

    hometask019 added

---
+++ HW019/Program.cs
@@ -0,0 +1,30 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void is_palindrome(int number)
+{
+    int first = number / 10000;
+    int second = number / 1000 % 10;
+    int fourth = number / 10 % 10;
+    int fifth = number % 10;
+
+    if ( first == fifth && second == fourth)
+    {
+        Console.WriteLine("Это палиндром!");
+    }
+    else
+    {
+        Console.WriteLine("Это не палиндром!");
+    }
+
+}
+
+int number = ReadData("Введите пятизначное число");
+
+is_palindrome(number);
+
+
+
