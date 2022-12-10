fd435440625f5128055fbea58950c96b60aef8d3
Lyubov <love.rodionova.spb@gmail.com>
2022-12-10 22:06:24 +0300

    hometask025 added

---
+++ HW025/Program.cs
@@ -0,0 +1,12 @@
+﻿int ReadNumber(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int A = ReadNumber("Введите число A");
+int B = ReadNumber("Введите число B");
+
+Console.WriteLine("A^B = " + Math.Pow(A,B));
+
+
