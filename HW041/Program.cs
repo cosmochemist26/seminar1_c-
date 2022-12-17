cf7ae533cc5bdd453305efa4d6c5aec28c5a3fce
Lyubov <love.rodionova.spb@gmail.com>
2022-12-17 20:59:28 +0300

    hometask041 added

---
+++ HW041/Program.cs
@@ -0,0 +1,34 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] ReadArray(string msg)
+{
+    Console.WriteLine(msg);
+
+    string Input = Console.ReadLine() ?? "0";
+    string[] SubStrings = Input.Split(", ");
+
+    List<int> Array = new List<int>();
+
+    foreach (var SubString in SubStrings) 
+    {
+        Array.Add(int.Parse(SubString));
+    }
+
+    return Array.ToArray();
+}
+
+int PosNumCount(int[] arr)
+{
+    int count = 0;
+    foreach (var num in arr)
+    {
+        if (num > 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int[] Array = ReadArray("Введите числа через запятую: ");
+Console.WriteLine("Количество чисел больше нуля: " + PosNumCount(Array).ToString());
+
